5638d4ede50862bed849db480619aa1df7ad5a21
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-03-16 13:08:43 +0530

    Added some meaningful comments for the index UserReducer file...

--- src/Reducers/UserReducer.js
+++ src/Reducers/UserReducer.js
@@ -1,8 +1,10 @@
+// This is setting initial state object for setting different state variables for this reducer...  
 const initialState = {
   allUsers: JSON.parse(localStorage.getItem("allUsers")) || [],
   userType: "Employee",
 };
 
+//  Reducer function that handles actions dispatched to the store does execution according to action call with or without payload..
 export const UserReducer = (state = initialState, action) => {
   switch (action.type) {
     case "REFRESHUSER": {
