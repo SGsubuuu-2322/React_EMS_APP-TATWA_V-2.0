2241e491e38d961c6fc56acb6b189beafc8deaf3
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-03-16 13:04:42 +0530

    Added some meaningful comments for the index reducer file...

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,6 +1,11 @@
+// This is importing the combineReducers method from the redux module....
 import { combineReducers } from "redux";
+
+// This is importing the userReducer from the redux module....
 import { UserReducer } from "./UserReducer";
 
+
+// This is setting the rootReducer and combine all the reducers into one. for storing it into the single redux store for our app....
 const rootRducer = combineReducers({
   users: UserReducer,
 });
