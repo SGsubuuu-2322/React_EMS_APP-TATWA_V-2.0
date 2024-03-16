da0e26aaa561ad8f4e1f76a3d4afe6faae607d42
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-03-16 10:23:02 +0530

    Added some meaningful comments to App component...

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 // import React from 'react'
+// This line is for importing Routing component for rendering it in App component...
 import Routing from "./Utils/Routing";
 
+// This is the App functional component which is responsible for rendering Routing component with some stylings applies using Tailwind...
 const App = () => {
   return (
     <div className="w-full h-screen bg-secondarylite flex flex-col">
@@ -9,4 +11,5 @@ const App = () => {
   );
 };
 
+// This is exporting App functional component so that we can use it elsewhere.
 export default App;
