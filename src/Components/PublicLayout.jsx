c2d34a053c4fd00c5ffac039fb7d99d36c613cac
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-03-16 10:56:01 +0530

    Added some meaningful comments, which explains the publiclayout functional component...

--- src/Components/PublicLayout.jsx
+++ src/Components/PublicLayout.jsx
@@ -1,12 +1,18 @@
 // import React from "react";
+// This is importing public navbar component for rendering purposes.
 import PublicNav from "./PublicNav";
+
+// This is importing outlet hook for rendering child components of the this parent component... 
 import { Outlet } from "react-router-dom";
 
+
+// This is public layout component which serves as a container for all public or unauthorized  routes or  pages in our app..
 const PublicLayout = () => {
   // console.log("Public layout is running...");
   return (
     <div>
       <PublicNav />
+      {/* This is where exactly child component coming as routes  will be rendered */}
       <Outlet />
     </div>
   );
