9a3a43956c9670c5b3dd2cd94481a0fe507acfe7
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-03-16 11:22:18 +0530

    Added some meaningful comments, which explains the publicRoutes js file...

--- src/Routes/PublicRoutes.js
+++ src/Routes/PublicRoutes.js
@@ -1,6 +1,8 @@
+// These are imports of Login and Register components...
 import Login from "../Pages/Login";
 import Register from "../Pages/Register";
 
+// This is the publicRoutes array for storing objects defining path and elements of Login and Register components...
 export const PublicRoutes = [
   {
     path: "/login",
