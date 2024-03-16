98e1e82775611b68d5192ac68f7dd6630efe2e65
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-03-16 11:17:07 +0530

    Added some meaningful comments, which explains the Layout component...

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,8 +1,13 @@
 // import React from 'react'
 
+// This is importing the Nav bar component for rendering the navigation bar...
 import Nav from "./Nav";
 
+// This is Layout functional component which receives props and renders out a layout with navbar, children (what gets put in between layout component...
 const Layout = ({ children }) => {
+  // This is taking children component by destructuring...
+
+  // This is rendering by returning different components in a layout form...
   return (
     <div className="w-full h-screen bg-secondarylite flex flex-col">
       <Nav />
