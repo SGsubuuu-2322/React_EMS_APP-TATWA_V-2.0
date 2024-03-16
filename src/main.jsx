3a8e26521ea812b796be9d61ac3100fb65a4b7b9
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-03-16 10:16:01 +0530

    Added some meaningful comments, which explains the code...

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,23 @@
 // import React from "react";
+// These are the some of the common imports for creating the main root element in the DOM...
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+
+// This is  for hookking up the routing feature of React in functional components...
 import { BrowserRouter } from "react-router-dom";
 // import Context from "./Utils/Context.jsx";
+// These two imports are for hooking up the single centralized store for our app created using redux and providing it to app component...
 import { Provider } from "react-redux";
 import store from "./Store.js";
 
+
+// This is for creating a new root element in DOM and rendering it....
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
   // <Context>
+  // These are for wrapping the root component with necessary data and rendering it...
+
   <Provider store={store}>
     <BrowserRouter>
       <App />
