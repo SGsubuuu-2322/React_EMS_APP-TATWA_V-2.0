6105c8be0544f4e97fcc85676f0659d1d6bd5d52
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-03-16 11:24:27 +0530

    Added some meaningful comments, which explains the authRoutes js file...

--- src/Routes/AuthRoutes.js
+++ src/Routes/AuthRoutes.js
@@ -1,6 +1,8 @@
+// These are imports of Home and UserProfile components...
 import Home from "../Pages/Home";
 import UserProfile from "../Pages/UserProfile";
 
+// This is the ProtectedRoutes array for storing objects defining path and elements of Home and UserProfile components...
 export const AuthRoutes = [
   {
     path: "/",
