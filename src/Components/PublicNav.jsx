b10f74483c9417916c6999f277de45b427635566
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-03-18 10:33:49 +0530

    Added some relevant comments for public navbar component...

--- src/Components/PublicNav.jsx
+++ src/Components/PublicNav.jsx
@@ -1,17 +1,27 @@
+// These are some imports of relevant functions from their respective hooks, which will help us to use react's core features...
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink, useLocation } from "react-router-dom";
 import { selectUser } from "../Actions";
 
+// This is navbar functional component that have structure and logic written in JSX to be ued in unAuthorized and unProtected routes...
 const PublicNav = () => {
+  // This is extracting location information from the useLocation hook...
   let location = useLocation();
+
+  // This is extracting the state data from users variable from central store of redux ...
   const { userType } = useSelector((state) => state.users);
+
+  // This is extracting the disPatch method from Redux useDispatch() hook to be used in actions.
   const dispatch = useDispatch();
 
+  // This is handling the dispatch action for changing the state data of central store according to the user choice of radio input...
   const handleOptionChange = (e) => {
     // console.log(e.target.value);
     dispatch(selectUser(e.target.value));
   };
   // console.log(location);
+
+  //  The navbar will show different options depending on if a user wants to login or register as a employee or admin
   return (
     <div className="w-full h-[12%] px-6 mb-6">
       <div className="content w-full bg-zinc-200 flex items-center justify-between px-3 overflow-y-hidden rounded-lg pb-1 border-primary border-b-4 shadow-md shadow-black ">
@@ -26,6 +36,7 @@ const PublicNav = () => {
           <h1 className="text-sky-900 text-2xl font-bold ">EMS</h1>
         </div>
 
+        {/* This is conditional rendering according to exact route location... */}
         {location.pathname === "/register" ? (
           <div className="button-holder h-20 w-40 bg-secondarylite border rounded-md flex justify-around items-center  border-2 overflow-hidden">
             <div className="admin-container h-20 w-10 flex flex-col justify-center gap-1">
@@ -36,12 +47,13 @@ const PublicNav = () => {
                   className="object-cover "
                 />
               </div>
+              {/* This radio input is checked after changing of state value from central redux store */}
               <input
                 type="radio"
                 name="admin"
                 value="Admin"
-                checked={userType === "Admin"}
                 onChange={handleOptionChange}
+                checked={userType === "Admin"}
               />
             </div>
             <div className="admin-container h-20 w-10 flex flex-col justify-center gap-1">
@@ -56,8 +68,8 @@ const PublicNav = () => {
                 type="radio"
                 name="employee"
                 value="Employee"
-                checked={userType === "Employee"}
                 onChange={handleOptionChange}
+                checked={userType === "Employee"}
               />
             </div>
           </div>
@@ -65,6 +77,7 @@ const PublicNav = () => {
           <></>
         )}
 
+        {/* These are some navlink with normal stylings and special stylings-which will shown when that specific navlink is active... */}
         <div className="navigation">
           <ul className="flex gap-10">
             <NavLink
